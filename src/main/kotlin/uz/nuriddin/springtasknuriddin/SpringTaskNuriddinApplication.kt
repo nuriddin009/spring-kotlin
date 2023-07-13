7d8f613899654808b00e14d145e0c076e12d92d5
Nuriddin <inoyatovnuriddin009@gmail.com>
2023-07-13 10:02:02 +0500

    first commit

---
+++ src/main/kotlin/uz/nuriddin/springtasknuriddin/SpringTaskNuriddinApplication.kt
@@ -0,0 +1,11 @@
+package uz.nuriddin.springtasknuriddin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTaskNuriddinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTaskNuriddinApplication>(*args)
+}
---
+++ src/test/kotlin/uz/nuriddin/springtasknuriddin/SpringTaskNuriddinApplicationTests.kt
@@ -0,0 +1,13 @@
+package uz.nuriddin.springtasknuriddin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTaskNuriddinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
